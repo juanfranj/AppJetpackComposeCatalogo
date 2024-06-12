ce065632bac3388d4b0f6d1db6247e0cddf23bbb
Juanfran <juanfranj@hotmail.com>
2024-06-13 00:42:05 +0200

    Incluido Firebase release y debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            applicationIdSuffix = ".debug"
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
